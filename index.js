24395d92f0918b5a7267442a17702d8e44bbe48f
Solomko2 <solomko2006@gmail.com>
2018-11-18 19:50:21 +0200

    Routin requests

--- index.js
+++ index.js
@@ -22,17 +22,48 @@ const server = http.createServer((req, res) => {
     // Get payload, if any
     const decoder = new StringDecoder('utf-8');
     let buffer = '';
+    // Define handlers
+    const handlers = {};
+    // Sample handler
+    handlers.sample = (data, callback) => {
+        callback(200, { message: 'Ok' });
+    };
+    // Not found handler
+    handlers.notFound = (data, callback) => {
+        callback(500);
+    };
+    // Define a request router
+    const router = {
+        'sample': handlers.sample,
+    };
     req.on('data', (data) => {
         buffer += decoder.write(data);
     });
     req.on('end', () => {
         buffer += decoder.end();
-        // Send the response
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Hello World!\n');
-        // Log the request path
-        console.log(`Request received with these payload: `, buffer);
+        const data = {
+            trimmedPath,
+            queryStringObject,
+            method,
+            headers,
+            payload: buffer
+        };
+        const chosenHandler = typeof (router[trimmedPath]) !== 'undefined'
+            ? handlers[trimmedPath]
+            : handlers.notFound;
+        chosenHandler(data, (statusCode, payload) => {
+            console.log(1, payload);
+            statusCode = typeof (statusCode) === 'number'
+                ? statusCode
+                : 200;
+            payload = typeof (payload) == 'object' ? payload : {};
+            // Convert payload to a string
+            const payloadStr = JSON.stringify(payload);
+            // return the response
+            res.writeHead(statusCode);
+            res.end(payloadStr);
+            console.log('Returning this response:  ', buffer);
+        });
     });
 });
 // Start the server and have it listen on port 3000
--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@
 // dependencies
 import * as http from 'http';
 import * as url from 'url';
-import {StringDecoder} from 'string_decoder'
+import {StringDecoder} from 'string_decoder';
 
 // the server should respond all requests with a string
 const server = http.createServer((req, res) => {
@@ -29,19 +29,58 @@ const server = http.createServer((req, res) => {
   const decoder = new StringDecoder('utf-8');
   let buffer = '';
 
+  // Define handlers
+  const handlers: any = {};
+
+  // Sample handler
+  handlers.sample = (data, callback) => {
+    callback(200, {message: 'Ok'});
+  };
+
+  // Not found handler
+  handlers.notFound = (data, callback) => {
+    callback(500);
+  };
+
+  // Define a request router
+  const router: any = {
+    'sample' : handlers.sample,
+  };
+
   req.on('data', (data) => {
     buffer += decoder.write(data);
   });
 
   req.on('end', () => {
     buffer += decoder.end();
-    // Send the response
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!\n');
 
-    // Log the request path
-    console.log(`Request received with these payload: `, buffer);
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      payload: buffer
+    };
+
+    const chosenHandler = typeof(router[trimmedPath]) !== 'undefined'
+      ? handlers[trimmedPath]
+      : handlers.notFound;
+
+    chosenHandler(data, (statusCode, payload) => {
+      console.log(1, payload);
+      statusCode = typeof(statusCode) === 'number'
+        ? statusCode
+        : 200;
+      payload = typeof(payload) == 'object' ? payload : {};
+
+      // Convert payload to a string
+      const payloadStr = JSON.stringify(payload);
+
+      // return the response
+      res.writeHead(statusCode);
+      res.end(payloadStr);
+      console.log('Returning this response:  ',buffer );
+    });
   });
 
 });
